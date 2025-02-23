f39083c61068c47595230330868befae54d1c940
Kseniya043 <vil-kse@ya.ru>
2025-02-23 18:55:23 +0300

    updated js

--- main.js
+++ main.js
@@ -26,7 +26,17 @@ let block= "main-documents__block" - как выбрать класс?
 
 main-documents__block_child
 3. использую if else написать логическое условие ( по 3 пункту)*/
+function getTranslateX(slide) { //- пишем функцию TranslateX с аргументом slide(любое название)
+    const style = window.getComputedStyle(slide);//получаем переменную, которая равна методу getComputedStyle с аргументом slide, который возвращает объект CSS после обработки всех стилей
+    const transform = style.transform; //- получаем переменную, которая равна переменной Style с ключем трансформ( из стилей)
+    if (!transform || transform === 'none') { //если не равен трансформ или трансформ строго равен строке
+        return 0; //- то возвращается 0
+    }
+    const match = transform.match(/matrix\([^,]+,[^,]+,[^,]+,[^,]+,\s*([^,]+),\s*([^,]+)\)/); //получаем переменную, каким то данным
+    return match ? parseFloat(match[1]) : 0; // возращает либо массив (match[2]), либо 0
+}
 
+console.log(document.getElementById('show-more-documents'));
 document.addEventListener('DOMContentLoaded', function () {
 // let button = document.querySelector('[id="show-more-documents"]')
 
@@ -40,25 +50,23 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    const buttonLeftSlide = document.getElementById('slider_btn-left')
-    const buttonRightSlide = document.getElementById('slider_btn-right')
-    const elementSlide = document.querySelector('.slider__block');
+    const buttonLeftSlide = document.getElementById('slider_btn-left') //-выбираем кнопку левую
+    const buttonRightSlide = document.getElementById('slider_btn-right')//-выбираем кнопку правую
+    const elementSlide = document.querySelector('.slider__block'); //- определяем родителя
 
-    const firstSlideElement = document.querySelector('.slider__block_reviews');
-    const widthSlide = firstSlideElement?.clientWidth
-    function getTranslateX(firstSlideElement) {
-        const style = window.getComputedStyle(firstSlideElement);
-        const transform = style.transform;
-        if (!transform || transform === 'none') {
-            return 0;
-        }
-        const match = transform.match(/matrix\([^,]+,[^,]+,[^,]+,[^,]+,\s*([^,]+),\s*([^,]+)\)/);
-        return match ? parseFloat(match[2]) : 0;
-    }
-    getTranslateX(document.getElementById('firstSlideElement'))
+    const firstSlideElement = document.querySelector('.slider__block_reviews'); //- определяем элемент класса слайда
+    const widthSlide = firstSlideElement?.clientWidth //- определяем ширину по переменной слайда
+
+    console.log(getTranslateX(elementSlide))
+    elementSlide.style.transform = `translateX(${widthSlide}px)`;
+    console.log(getTranslateX(elementSlide))
+
 })
 
-
+//Что такое аргумент функции? Что такое массив? шаблонное выражение ${}, книжку прочитать еще
+// типы данных, как работать с htm элементами
+// создать переменную Х = getTranslateX(elementSlide) - или + widthSlide
+//  elementSlide.style.transform = `translateX(${Х}px)`;
 
 
 
